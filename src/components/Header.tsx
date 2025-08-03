bf0faa9dfa2d0814082fc4cdd3ea83924eff37e3
amyrneto <amyrneto@gmail.com>
2025-08-03 11:41:04 -0300

    Click the logo to go to Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 type HeaderProps = {
   logoSrc: string;
@@ -10,7 +11,13 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ logoSrc, children, height = '120px' }) => (
   <header className={styles.header} style={{ height }}>
     <div className={styles.logoContainer} style={{ maxWidth: height, marginLeft: '2rem', marginTop: '1rem' }}>
-      <img src={logoSrc} alt="Logo" className={styles.logo} />
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <img 
+          src={logoSrc} 
+          alt="Amyr Showcase Logo" 
+          style={{ cursor: 'pointer' }}
+        />
+      </Link>
     </div>
     <div className={styles.switchTheme}>
       {children}
